8e308d31f4955155537188a8c600a44bd94d16f6
Maurizio Vitale <mav@polymath-solutions.com>
2022-03-31 20:04:30 -0400

    WIP

--- test/runner.go
+++ test/runner.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func main() {
 	fmt.Printf("Runner")
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			fmt.Println(path, info.Size())
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
 }
