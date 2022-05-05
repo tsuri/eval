8d8675fec567801b14a86f13f4e575cab5184273
Maurizio Vitale <mav@polymath-solutions.com>
2022-05-04 20:31:22 -0400

    WIP

--- cmd/builder/main.go
+++ cmd/builder/main.go
@@ -189,7 +189,7 @@ func build(branch string, commitSHA string, targets []string) {
 							Args: []string{"--insecure",
 								"--insecure-pull",
 								"--skip-tls-verify",
-								"--build-arg", "TARGETS=something",
+								"--build-arg", "TARGETS=something and more",
 								"--destination=registry.other.net:5000/test:bar",
 								"--context", gitContext,
 								"--dockerfile=dockerfile"},
