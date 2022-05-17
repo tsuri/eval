5bd0de4d62f8a5302dad064e8cb018649928a8e4
Maurizio Vitale <mav@polymath-solutions.com>
2022-05-16 22:33:06 -0400

    WIP

---
+++ pkg/types/types.go
@@ -0,0 +1,26 @@
+package types
+
+import pbtypes "eval/proto/types"
+
+func scalarType(t pbtypes.Type_AtomicType) *pbtypes.Type {
+	return &pbtypes.Type{
+		Impl: &pbtypes.Type_Atomic{t},
+	}
+}
+
+func scalarValueString(s string) []*pbtypes.FieldValue {
+	return []*pbtypes.FieldValue{
+		{
+			Value: &pbtypes.ScalarValue{
+				Value: &pbtypes.ScalarValue_S{s},
+			},
+		},
+	}
+}
+
+func StringScalar(s string) *pbtypes.TypedValue {
+	return &pbtypes.TypedValue{
+		Type:   scalarType(pbtypes.Type_STRING),
+		Fields: scalarValueString(s),
+	}
+}
